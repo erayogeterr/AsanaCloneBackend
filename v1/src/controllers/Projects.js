07afcf37b52c8e213e78565373b5ccafd22c967e
Eray Ögeter <erayogeterr@gmail.com>
2023-03-11 17:57:22 +0300

    db connect

--- v1/src/controllers/Projects.js
+++ v1/src/controllers/Projects.js
@@ -3,12 +3,12 @@ const httpStatus = require("http-status");
 
 const create = (req, res) => {
     insert(req.body)
-    .then((response) => {
+    .then((response) => { 
         res.status(httpStatus.CREATED).send(response);
-    }).catch((e) => {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e);
     })
-
+    .catch((e) => {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e)
+    })
 }
 
 const index = (req, res) => {
