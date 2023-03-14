2438524fd383f64eb7e292f0dd19569085d2ac08
Eray Ögeter <erayogeterr@gmail.com>
2023-03-14 11:50:32 +0300

    project update feature and some changes.

--- v1/src/controllers/Projects.js
+++ v1/src/controllers/Projects.js
@@ -1,7 +1,8 @@
-const { insert, list } = require("../services/Projects")
+const { insert, modify, list } = require("../services/Projects")
 const httpStatus = require("http-status");
 
 const create = (req, res) => {
+    req.body.user_id = req.user;
     insert(req.body)
     .then((response) => { 
         res.status(httpStatus.CREATED).send(response);
@@ -17,7 +18,19 @@ const index = (req, res) => {
     }).catch(e => res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e));
 }
 
+const update = (req, res) => {
+    if(!req.params?.id) {
+       return res.status(httpStatus.BAD_REQUEST).send({
+        message : "ID bilgisi eksik."
+    });
+    } 
+    modify(req.body, req.params?.id).then(updatedProject => {
+        res.status(httpStatus.OK).send(updatedProject)
+    }).catch(e => res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error : "Kayıt sırasında bir problem oluştu."}))
+};
+
 module.exports = {
     create,
     index,
+    update,
 }
