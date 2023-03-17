814c702749a7ecfaa007bc777cecd7dcbf978fc3
Eray Ögeter <erayogeterr@gmail.com>
2023-03-17 11:36:35 +0300

    added project deleted service.

--- v1/src/controllers/Projects.js
+++ v1/src/controllers/Projects.js
@@ -1,4 +1,4 @@
-const { insert, modify, list } = require("../services/Projects")
+const { insert, modify, list, remove } = require("../services/Projects")
 const httpStatus = require("http-status");
 
 const create = (req, res) => {
@@ -29,8 +29,26 @@ const update = (req, res) => {
     }).catch(e => res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error : "Kayıt sırasında bir problem oluştu."}))
 };
 
+const deleteProject = (req, res ) => {
+    if(!req.params?.id) {
+        return res.status(httpStatus.BAD_REQUEST).send({
+         message : "ID bilgisi eksik."
+     });
+     } 
+
+     remove(req.params?.id).then(deletedProject => {
+        if(!deletedProject) {
+           return res.status(httpStatus.NOT_FOUND).send({
+                message : "Kayıt bulunamadı."
+            })
+        }
+        res.status(httpStatus.OK).send({message : "Proje silinmiştir."})
+    }).catch(e => res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error : "Silme sırasında bir problem oluştu."}))
+}
+
 module.exports = {
     create,
     index,
     update,
+    deleteProject,
 }
